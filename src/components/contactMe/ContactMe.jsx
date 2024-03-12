c8c7b1c089d09cd070352e0d0928c170b5829912
Danielle Soares <silvasoaresdanielle2@gmail.com>
2024-03-11 21:17:59 -0300

    web3forms API added

--- src/components/contactMe/ContactMe.jsx
+++ src/components/contactMe/ContactMe.jsx
@@ -7,8 +7,55 @@ import rightAr from '../../assets/images/contact me vectors/pattern_right/ar.png
 import rightCel from '../../assets/images/contact me vectors/pattern_right/cel.png'
 import rightTel from '../../assets/images/contact me vectors/pattern_right/tel.png'
 import rightEmail from '../../assets/images/contact me vectors/pattern_right/email.png'
+import { useForm } from "react-hook-form";
+import useWeb3Forms from "@web3forms/react";
+import {useRef, useState, useEffect} from 'react';
 
 function ContactMe() {
+    const formRef = useRef(null)
+    const newMessageButtonRef = useRef(null)
+    const paragraphRef = useRef(null)
+    const titleRef = useRef(null)
+
+    const {register, reset, handleSubmit} = useForm();
+
+    const [isSuccess, setIsSuccess] = useState(false);
+    const [result, setResult] = useState(null);
+
+    const accessKey = "d4713433-476f-46db-a5d1-692adc33f8d5";
+
+    const messageSended = () => {
+        formRef.current.style.display = "none";
+        newMessageButtonRef.current.style.display="block";
+        titleRef.current.innerHTML = "Mensagem enviada com sucesso!";
+        paragraphRef.current.innerHTML = "Responderei o quanto antes e entrarei em contato ;)"
+    }
+    
+    const newMessage = () => {
+        formRef.current.style.display = "flex";
+        newMessageButtonRef.current.style.display="none";
+        titleRef.current.innerHTML = "Entre em contato";
+        paragraphRef.current.innerHTML = "Vamos trabalhar juntos? <br/>Sinta-se livre para me mandar uma mensagem."
+    }
+
+    const { submit: onSubmit } = useWeb3Forms({
+        access_key: accessKey,
+        settings: {
+          from_name: "Acme Inc",
+          subject: "New Contact Message from your Website",
+        },
+        onSuccess: (msg, data) => {
+          setIsSuccess(true);
+          setResult(msg);
+          reset();
+        },
+        onError: (msg, data) => {
+          setIsSuccess(false);
+          setResult(msg);
+        },
+    });
+
+
     return (
         <div className="ContactMe_container">
             <div className="icons_container left_icons_container">
@@ -18,21 +65,24 @@ function ContactMe() {
                 <img src={leftEmail} class="left_email left_icon"></img>
             </div>
             <div className="ContactMe_main_container">
-                <h2>Entre em contato</h2>
-                <p> 
+                <h2 useRef={titleRef}>Entre em contato</h2>
+                <p useRef={paragraphRef}> 
                     Vamos trabalhar juntos? <br/>
                     Sinta-se livre para me mandar uma mensagem.
                 </p>
-                <div className="inputs_container">
-                <label className="field">Seu nome</label>
-                <input type="text"/>
-                
-                <label className="field">Seu email</label>
-                <input type="email"/>
-                <label className="field">Mensagem</label>
-                <input type="text" class="message_input"/>
-                </div>
-                <button className="submit_button">Enviar</button>
+                <form useRef={formRef} onSubmit={handleSubmit(onSubmit)} className="inputs_container">
+                    <label className="field">Seu nome</label>
+                    <input type="text" {...register("name", { required: true })}/>
+                    
+                    <label className="field">Seu email</label>
+                    <input type="email" {...register("email", { required: true })}/>
+                    
+                    <label className="field">Mensagem</label>
+                    <textarea class="message_input" {...register("message", { required: true })}></textarea>
+
+                    <button type="submit" className="submit_button">Enviar</button>
+                </form>
+                <button ref={newMessageButtonRef} onClick={newMessage} className="new_message_button submit_button">Enviar nova mensagem</button>
             </div>
             <div className="icons_container right_icons_container">
                 <img src={rightAr} class="right_ar right_icon"></img>
